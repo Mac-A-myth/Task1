dd52a5e9a241e5f214454ae7bdf7175e57045312
Mac-A-myth <104204199+Mac-A-myth@users.noreply.github.com>
2025-11-08 07:54:57 +0530

    Refactor sorting algorithms to use abstract class

--- SortingAlgorithm.java
+++ SortingAlgorithm.java
@@ -1,79 +1,92 @@
-// Simple sorting interface
-interface SortingAlgorithm {
-    void sort(int[] array);
-    String getAlgorithmName();
+// Abstract class
+abstract class Sorting {
+    // Abstract method to be implemented by subclasses
+    abstract void sort(int[] arr);
+
+    // Common method to display the array
+    public void display(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
 }
 
-// Bubble Sort implementation
-class BubbleSort implements SortingAlgorithm {
+// Subclass 1: Bubble Sort
+class BubbleSort extends Sorting {
     @Override
-    public void sort(int[] array) {
-        int n = array.length;
+    void sort(int[] arr) {
+        int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    // Swap elements
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
-    }
-    
-    @Override
-    public String getAlgorithmName() {
-        return "Bubble Sort";
+        System.out.println("Bubble Sort Result:");
+        display(arr);
     }
 }
 
-// Insertion Sort implementation
-class InsertionSort implements SortingAlgorithm {
+// Subclass 2: Selection Sort
+class SelectionSort extends Sorting {
     @Override
-    public void sort(int[] array) {
-        int n = array.length;
-        for (int i = 1; i < n; i++) {
-            int key = array[i];
-            int j = i - 1;
-            
-            // Move elements greater than key to one position ahead
-            while (j >= 0 && array[j] > key) {
-                array[j + 1] = array[j];
-                j = j - 1;
-            }
-            array[j + 1] = key;
-        }
-    }
-    
-    @Override
-    public String getAlgorithmName() {
-        return "Insertion Sort";
-    }
-}
-
-// Selection Sort implementation
-class SelectionSort implements SortingAlgorithm {
-    @Override
-    public void sort(int[] array) {
-        int n = array.length;
+    void sort(int[] arr) {
+        int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
-            // Find the minimum element in unsorted array
             int minIndex = i;
             for (int j = i + 1; j < n; j++) {
-                if (array[j] < array[minIndex]) {
+                if (arr[j] < arr[minIndex]) {
                     minIndex = j;
                 }
             }
-            
-            // Swap the found minimum element with the first element
-            int temp = array[minIndex];
-            array[minIndex] = array[i];
-            array[i] = temp;
+            // swap arr[minIndex] and arr[i]
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
         }
+        System.out.println("Selection Sort Result:");
+        display(arr);
     }
-    
+}
+
+// Subclass 3: Insertion Sort
+class InsertionSort extends Sorting {
     @Override
-    public String getAlgorithmName() {
-        return "Selection Sort";
+    void sort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Move elements greater than key to one position ahead
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+        System.out.println("Insertion Sort Result:");
+        display(arr);
+    }
+}
+
+// Main class to test all sorts
+public class SortingDemo {
+    public static void main(String[] args) {
+        int[] data = {64, 25, 12, 22, 11};
+
+        Sorting bubble = new BubbleSort();
+        Sorting selection = new SelectionSort();
+        Sorting insertion = new InsertionSort();
+
+        // Each subclass sorts its own copy
+        bubble.sort(data.clone());
+        selection.sort(data.clone());
+        insertion.sort(data.clone());
     }
 }
